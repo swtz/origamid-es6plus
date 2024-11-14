adcf318f0ddca997591c4453956dcacc9d3e2810
Leonardo Lüders <luderser@hotmail.com>
2024-11-14 16:52:44 -0300

    termino dos exercicios 0401

--- script-aula1.js
+++ script-aula1.js
@@ -1,27 +1,36 @@
-// Objetos -> 0401 Constructor Functions 2
-
-// objeto puro sem construtor
-// const Dom = {
-//   seletor: 'li',
-//   element() {
-//     return document.querySelector(this.seletor);
-//   },
-//   ativar() {
-//     this.element().classList.add('ativar');
-//   }
-// }
-
-// com func construtora
-function Dom(seletor) {
-  this.element = function () {
-    return document.querySelector(seletor);
-  };
-  this.ativar = function () {
-    this.element().classList.add('ativar');
+// #1
+const pessoa = {
+  nome: 'Leonardo',
+  idade: 0,
+  andar() {
+    console.log(this.nome + ' andou');
   }
 }
 
-const li = new Dom('li');
-const ul = new Dom('ul');
-const lastLi = new Dom('li:last-child');
-lastLi.ativar();
+// #1 my solve
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+  this.andar = function () {
+    console.log(this.nome + ' andou');
+  }
+}
+
+const p1 = new Pessoa('João', 20);
+const p2 = new Pessoa('Maria', 25);
+const p3 = new Pessoa('Bruno', 15);
+
+// #2 my solve
+function Dom(element) {
+  const elementsList = document.querySelectorAll(element);
+  this.elements = elementsList;
+  this.addClass = function (classe) {
+    elementsList.forEach(function (item) { item.classList.add(classe) });
+  };
+  this.removeClass = function (classe) {
+    elementsList.forEach((item) => item.classList.remove(classe));
+  };
+}
+
+const nodeListLi = new Dom('li');
+nodeListLi.addClass('ativar');
