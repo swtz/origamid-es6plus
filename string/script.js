7f6a072ea29dd1884d69606852438e876c7b5c48
Leonardo Lüders <luderser@hotmail.com>
2024-12-06 16:09:35 -0300

    termino dos exercicios 0404

--- string/script.js
+++ string/script.js
@@ -1,34 +1,71 @@
-const nome = 'Pizza';
-const rua = new String('Rio de Janeiro');
-const idade = new String(25);
+// #1
+const transacoes = [
+    {
+        descricao: 'Taxa do pão',
+        valor: 'R$ 39',
+    },
+    {
+        descricao: 'Taxa do mercado',
+        valor: 'R$ 129',
+    },
+    {
+        descricao: 'Recebimento de cliente',
+        valor: 'R$ 99',
+    },
+    {
+        descricao: 'Taxa do banco',
+        valor: 'R$ 129',
+    },
+    {
+        descricao: 'Recebimento de cliente',
+        valor: 'R$ 49',
+    },
+];
 
-nome.includes('a', nome.length - 1); // true | 2nd arg set the initial index to search | CS
+let totalTaxa = 0;
+let totalRecebimento = 0;
 
-const phoneNumber = '48996642905';
-const phoneNumberLastFourDigits = phoneNumber.slice(-4); // Corta a string de acordo com os valores de start e end
+transacoes.forEach((item) => {
+    const valor = item.valor;
+    const valorLimpo = +valor.replace('R$ ', '');
 
-console.log(phoneNumberLastFourDigits); // 2905
-'string'.substring(); // funciona igual o .slice, porém não suporta valores negativos
-
-const email = "luderleo#email.com"
-console.log(email.indexOf("@"));
-
-if (email.indexOf("@") === -1) {
-    console.log('email inválido'); // .indexOf find first value | .lastIndexOf find last value
-}
-
-const registro = "45";
-const registroFormatado = registro.padStart(5, '0'); // 00045
-
-String.prototype.replace();
-
-const tagMeta = document.querySelectorAll('meta');
-let strMeta = '';
-
-tagMeta.forEach((item) => {
-    strMeta += item.outerHTML + ' '
+    if (item.descricao.toLowerCase().trim().startsWith('tax')) {
+        totalTaxa += valorLimpo;
+    } else if (item.descricao.toLowerCase().trim().startsWith('receb')) {
+        totalRecebimento += valorLimpo;
+    }
 });
 
-console.log(strMeta);
-console.log(strMeta.split('meta'));
-console.log(strMeta.split('meta').join('h1'));
+console.log(totalTaxa);
+console.log(totalRecebimento);
+
+// 2#
+
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+const transacoesArray = transportes.split(';');
+console.log(transacoesArray);
+
+// #3
+
+const html = `
+    <ul>
+        <li><span>Sobre</span></li>
+        <li><span>Produtos</span></li>
+        <li><span>Contatos</span></li>
+    </ul>
+`
+
+console.log(html.split('span').join('a'));
+
+// #4
+const frase = 'Melhor do ano!';
+console.log('#4 =>', frase.slice(-1));
+
+// #5
+const transacoes2 = ['Taxa do Banco', '     TAXA DO PÃO', '    taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+let count = 0;
+transacoes2.forEach((item) => {
+    if (item.toLocaleLowerCase().trim().includes('taxa')) count++;
+});
+
+console.log(count); // 3
