493a0c6a32422a0f75ed6baf6b53f14b6235e2e9
Leonardo Lüders <luderser@hotmail.com>
2024-12-12 21:03:16 -0300

    this: [].reduce() | termino da aula 0407 pt1

--- number-math/script.js
+++ number-math/script.js
@@ -1,55 +1,15 @@
-const carros = ['Ford', 'Fiat', 'Honda'];
+const aulas = [10, 25, 30];
 
-const retornoForEach = carros.forEach((item, index, array) => {
-    console.log(item);
+const reduceAulas = aulas.reduce((acumulador, item, index, array) => {
+    return acumulador + item;
+}, 0);
+
+// console.log(reduceAulas);
+
+const numeros = [10, 25, 60, 5, 35, 10];
+
+const maiorValor = numeros.reduce((prev, current) => {
+    return prev < current ? current : prev;
 });
 
-console.log('ForEach: ', retornoForEach);
-
-const pessoas = ['Leonardo', 'Laura', 'Maria', 'Giovana', 'Letícia'];
-
-const retornoMap = pessoas.map((item, index, array) => {
-    return item.toUpperCase();
-});
-
-console.log(retornoMap);
-
-const numbers = [45, 33, 2, 6, 88, 3];
-
-const numbersx2 = numbers.map((item) => {
-    return item * 2;
-});
-
-console.log(numbersx2);
-
-const aulas = [
-    {
-        nome: 'HTML 1',
-        min: 15
-    },
-    {
-        nome: 'HTML 2',
-        min: 10
-    },
-    {
-        nome: 'CSS 1',
-        min: 20
-    },
-    {
-        nome: 'JS 1',
-        min: 25
-    },
-];
-
-// #1
-// function getMinutes(aula) {
-//     return aula.min;
-// };
-
-
-//#2
-const getMinutes = aula => aula.min;
-
-const classDuration = aulas.map(getMinutes);
-
-console.log(classDuration);
+console.log(maiorValor);
