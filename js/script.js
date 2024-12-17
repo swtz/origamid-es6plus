dcab81660418714b0af0aa9a85f113e02f7efaef
Leonardo Lüders <luderser@hotmail.com>
2024-12-17 16:17:33 -0300

    termino da aula 0502 pt1 | modularizacao do projeto | export default | named export

---
+++ js/script.js
@@ -0,0 +1,11 @@
+import initChangeJsToDataAttr from './modules/class-to-dataset.js';
+import initScrollSuave from './modules/scroll-smooth.js';
+import initAnimacaoScroll from './modules/scroll-animation.js';
+import initAccordion from './modules/accordion-list.js';
+import initTabNav from './modules/tabnav.js';
+
+initChangeJsToDataAttr();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
