96e9c834223dbfbc0d970a118021ca2ef8da790a
Leonardo Lüders <luderser@hotmail.com>
2024-12-17 20:54:56 -0300

    class 0503 partially completed | Modal | javascript

---
+++ js/modules/modal.js
@@ -0,0 +1,7 @@
+export default function initModal() {
+}
+
+const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+const botaoFechar = document.querySelector('[data-modal="fechar"]');
+const containerModal = document.querySelector('[data-modal="container"]');
+
--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initScrollSuave from './modules/scroll-smooth.js';
 import initAnimacaoScroll from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion-list.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
 
 initChangeJsToDataAttr();
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
