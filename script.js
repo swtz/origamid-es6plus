5219701f690f05525a83350052f6d03948dd3b65
Leonardo Lüders <luderser@hotmail.com>
2024-12-17 10:38:55 -0300

    termino da aula 0501 | HTMLElement.dataset

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Efeitos no DOM
+const div = document.querySelector('[data-color]');
+
+div.dataset.fontSize = '6rem';
+
+console.log(div.dataset); // { color: 'blue' }
+console.log(div.getAttributeNames());
