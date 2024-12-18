b13863d87e7481560f33d9738cf17e75beeadd5b
Leonardo Lüders <luderser@hotmail.com>
2024-12-18 10:21:48 -0300

    termino da aula 0503 | Modal | javascript | this | toggle

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,7 +1,21 @@
 export default function initModal() {
+  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+  const botaoFechar = document.querySelector('[data-modal="fechar"]');
+  const containerModal = document.querySelector('[data-modal="container"]');
+
+  if (botaoAbrir && botaoFechar && containerModal) {
+
+    function handleModal(event) {
+      event.preventDefault();
+      containerModal.classList.toggle('ativo');
+    }
+
+    function clickOutside(event) {
+      (event.target === this) ? handleModal(event) : null;
+    }
+
+    botaoAbrir.addEventListener('click', handleModal);
+    botaoFechar.addEventListener('click', handleModal);
+    containerModal.addEventListener('click', clickOutside);
+  }
 }
-
-const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-const botaoFechar = document.querySelector('[data-modal="fechar"]');
-const containerModal = document.querySelector('[data-modal="container"]');
-
