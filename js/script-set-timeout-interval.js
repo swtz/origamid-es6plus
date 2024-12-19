6dbd761d83737e9ef4afe592deef9febe930d79e
Leonardo Lüders <luderser@hotmail.com>
2024-12-19 11:13:17 -0300

    termino da aula 0508 pt1 | setInterval | clearInterval(arg: setInterval)

--- js/script-set-timeout-interval.js
+++ js/script-set-timeout-interval.js
@@ -1,10 +1,17 @@
-const btn = document.querySelector('button');
+// # setInterval
 
-btn.addEventListener('click', handleClick);
+// const time = 1000;
+// setInterval((text) => {
+//   console.log(text);
+// }, time, `Print this text every ${time} second`);
 
-function handleClick() {
-  setTimeout(() => {
-    console.log(this);
-    this.classList.add('ativo');
-  }, 1000);
+let i = 0;
+const until10 = setInterval(callbackFn, 300);
+
+function callbackFn() {
+  console.log(i++);
+  if (i > 10) {
+    clearInterval(until10);
+    console.log('This is the end');
+  }
 }
