1a41926b8aefeed2d5cecc63d0809d2179d6af23
Leonardo Lüders <luderser@hotmail.com>
2024-12-20 11:55:08 -0300

    termino dos exercicios 0508 | stopwatch w/ setInterval | nao corrigido

--- js/script-set-timeout-interval.js
+++ js/script-set-timeout-interval.js
@@ -1,17 +1,72 @@
-// # setInterval
+// #1
+const html = document.querySelector('html');
 
-// const time = 1000;
-// setInterval((text) => {
-//   console.log(text);
-// }, time, `Print this text every ${time} second`);
+// setInterval(() => {
+//   if (html.classList.contains('red')) {
+//     html.classList.remove('red');
+//     html.classList.add('blue');
+//   } else {
+//     html.classList.remove('blue');
+//     html.classList.add('red');
+//   }
+// }, 2000)
 
-let i = 0;
-const until10 = setInterval(callbackFn, 300);
+// #2
+const btnInit = document.querySelector('[data-stopwatch="init"]');
+const btnPause = document.querySelector('[data-stopwatch="pause"]');
+const timerText = document.querySelector('p');
 
-function callbackFn() {
-  console.log(i++);
-  if (i > 10) {
-    clearInterval(until10);
-    console.log('This is the end');
+btnInit.addEventListener('click', handleClick);
+
+let timer = 0;
+
+function handleClickMyself() {
+  const stopwatch = setInterval(() => {
+    timerText.innerText = timer++;
+  }, 300)
+  btnInit.removeEventListener('click', handleClick);
+
+  function pauseTimer() {
+    clearInterval(stopwatch);
+    this.removeEventListener('click', pauseTimer);
   }
+  btnPause.addEventListener('click', pauseTimer);
+
+  function cleanTimer() {
+    clearInterval(stopwatch);
+    timer = 0;
+    timerText.innerText = timer;
+    btnPause.removeEventListener('dblclick', cleanTimer);
+  }
+  btnPause.addEventListener('dblclick', cleanTimer)
+}
+
+// # Otimizado pelo GitHub Copilot
+function startTimer() {
+  return setInterval(() => {
+    timerText.innerText = timer++;
+  }, 300);
+}
+
+function pauseTimer(stopwatch) {
+  return function () {
+    clearInterval(stopwatch);
+    this.removeEventListener('click', pauseTimer(stopwatch));
+  }
+}
+
+function cleanTimer(stopwatch) {
+  return function () {
+    clearInterval(stopwatch);
+    timer = 0;
+    timerText.innerText = timer;
+    btnPause.removeEventListener('dblclick', cleanTimer(stopwatch));
+  }
+}
+
+function handleClick() {
+  const stopwatch = startTimer();
+  btnInit.removeEventListener('click', handleClick);
+  btnPause.addEventListener('click', pauseTimer(stopwatch));
+  btnPause.addEventListener('dblclick', cleanTimer(stopwatch));
 }
