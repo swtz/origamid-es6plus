8a45df1ba20e6730146242b6ad2b86d21576e5dc
Leonardo Lüders <luderser@hotmail.com>
2024-12-23 20:11:03 -0300

    termino dos exercicios 0603 | Fetch API | corrigido

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -1,55 +1,45 @@
 // #1
-const form = document.querySelector('#cepform');
-const btn = form.querySelector('button');
-const div = document.querySelector('[data-show]');
-btn.addEventListener('click', handleClick);
+const inputCep = document.getElementById('cep');
+const btnCep = document.querySelector('input[type="button"]');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+btnCep.addEventListener('click', handleClick);
 
 function handleClick(event) {
   event.preventDefault();
-  const inputValue = form.cep.value;
-  const cep = fetch(`https://viacep.com.br/ws/${inputValue}/json/`);
-  cep.then(response => response.text().then(result => {
-    div.innerText = result;
-  })
-  )
+  const cep = inputCep.value;
+  buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.text())
+    .then(result => resultadoCep.innerText = result)
 }
 
 // #2
-// const span = div.querySelector('span');
+const p = document.querySelector('.btc');
 
-// setInterval(() => {
-//   const bitcoin = fetch('https://blockchain.info/ticker')
-
-
-//   bitcoin.then(response => response.json().then(result => {
-//     getBitcoinPrice(result);
-//   }))
-// }, 1000)
-
-// let i = 0;
-
-// function getBitcoinPrice(result) {
-//   const brl = result.BRL;
-//   const buyPrice = brl.buy;
-//   const cleanPrice = buyPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-//   span.innerText = cleanPrice;
-//   const p = document.createElement('p');
-//   p.innerText = i++ + 'seg';
-//   span.appendChild(p);
-// }
-
+function fetchBtc() {
+  fetch('https://blockchain.info/ticker')
+    .then(response => response.json())
+    .then(result => {
+      const brl = result.BRL.buy.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+      p.innerText = brl;
+    })
+}
+// setInterval(fetchBtc, 30000);
 
 // #3
-// const h3 = div.querySelector('h3');
-// const next = div.querySelector('button');
-// next.addEventListener('click', handleClick);
+const pJoke = document.querySelector('.joke');
+const btnJoke = document.querySelector('.next');
 
-// function handleClick() {
-//   const data = fetch('https://api.chucknorris.io/jokes/random');
-//   data.then(response => response.json().then(result => {
-//     const piada = result.value;
-//     h3.innerText = piada;
-//   }))
-// }
+btnJoke.addEventListener('click', getJoke)
 
-// next.click();
+function getJoke() {
+  fetch('https://api.chucknorris.io/jokes/random')
+    .then(response => response.json())
+    .then(result => pJoke.innerText = result.value)
+}
+
+getJoke();
