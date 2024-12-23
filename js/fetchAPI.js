8549c6a4887138f94203135660ba8878d77d0ba5
Leonardo Lüders <luderser@hotmail.com>
2024-12-23 17:24:34 -0300

    termino da aula 0603 pt1 | Fetch API

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -1,20 +1,27 @@
-const cep = fetch('https://viacep.com.br/ws/01001000/json/');
+// const cep = fetch('https://viacep.com.br/ws/01001000/json/');
+// cep.then(response => response.json()).then(body => {
+//   const content = document.querySelector('.content');
+//   Object.entries(body).forEach((item, index, array) => {
+//     const arr = array[index];
+//     const title = arr[0];
+//     const description = arr[1];
+//     console.log(title, description);
 
-cep.then(response => response.json()).then(body => {
+//     const h2 = document.createElement('h2');
+//     const span = document.createElement('span');
+
+//     h2.innerText = title
+//     span.innerText = description
+
+//     content.appendChild(h2);
+//     content.appendChild(span);
+//   })
+// })
+
+const about = fetch('./about.html');
+
+about.then(response => response.text())
+.then(result => {
   const content = document.querySelector('.content');
-  Object.entries(body).forEach((item, index, array) => {
-    const arr = array[index];
-    const title = arr[0];
-    const description = arr[1];
-    console.log(title, description);
-
-    const h2 = document.createElement('h2');
-    const span = document.createElement('span');
-
-    h2.innerText = title
-    span.innerText = description
-
-    content.appendChild(h2);
-    content.appendChild(span);
-  })
+  content.innerHTML = result;
 })
