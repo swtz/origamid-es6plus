4eb30c2248db5abe98af038aa2f8cb3df2aa869e
Leonardo Lüders <luderser@hotmail.com>
2025-01-04 09:17:00 -0300

    termino da aula 0702 Get and Set | 'não é possível ter um 'getter' sem um 'setter' (o inverso é permitido)

---
+++ js/script-get-set.js
@@ -0,0 +1,20 @@
+class Button {
+  constructor(text, color) {
+    this.text = text;
+    this.color = color;
+  }
+
+  get element() {
+    const type = this._elementType || 'button';
+    const btnElement = document.createElement(type);
+    btnElement.innerText = this.text;
+    btnElement.style.color = this.color;
+    return btnElement;
+  }
+
+  set element(value) {
+    this._elementType = value;
+  }
+}
+const blueBtn = new Button('Comprar', 'blue');
+blueBtn.element = 'a';
