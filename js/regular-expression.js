75e13302012ba399e2ada226c501de25a74eaaa8
Leonardo Lüders <luderser@hotmail.com>
2025-01-07 11:19:27 -0300

    termino da aula 0901 Regexp Selection | /<regex-expression>/<regex-flag>;

---
+++ js/regular-expression.js
@@ -0,0 +1,8 @@
+// Tip: sempre consultar o material de apoio ao
+// usar o regex de fato.
+
+const frase = 'JavaScript';
+const regexp = /J/;
+const fraseNova = frase.replace(regexp, 'B');
+
+console.log(fraseNova);
