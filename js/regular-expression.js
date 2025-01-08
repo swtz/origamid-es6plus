ac4e3154d7022ad0f8af5bd296ed876f382f22f0
Leonardo Lüders <luderser@hotmail.com>
2025-01-08 11:33:05 -0300

    termino da aula 0904 Methods | String.prototype.replace(RegExp, (...args) => String)

--- js/regular-expression.js
+++ js/regular-expression.js
@@ -1,10 +1,21 @@
-// Methods
+// Methods pt2
 
-const frase = 'JavaScript, TypeScript, CoffeeScript, Java';
+const email = `
+empresa@email.com
+contato@email.com
+suporte@email.com
+financeiro@prefeiturabalneariogaivota.sc.gov.br
+`;
 
-const regexp = /\w+/g;
+const regexp = /(\w+)(@[\w.]+)/g;
 
-let regexResult;
-while ((regexResult = regexp.exec(frase)) !== null) {
-  console.log(regexResult);
-}
+const result = email.replace(regexp, (...args) => {
+  const grupo1 = args[1];
+  const grupo2 = args[2];
+  const emailPattern = '@prefeiturabalneariogaivota.sc.gov.br';
+  if (grupo2 !== emailPattern) {
+    return `${grupo1}${emailPattern}`;
+  };
+  return args[0]; // capturaGeral, ou seja, não caiu na condicional
+});
+console.log(result);
