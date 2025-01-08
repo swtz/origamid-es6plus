93f62e9b961ed5874a7c724faeb5c972777b9da1
Leonardo Lüders <luderser@hotmail.com>
2025-01-08 20:06:06 -0300

    termino da aula 0906 Validar CPF pt1 & pt2 | obs. do operador lógico && (AND)

---
+++ js/script-validar-cpf.js
@@ -0,0 +1,6 @@
+import ValidarCPF from "./validar-cpf.js";
+
+const cpf = document.querySelector('#cpf');
+const validarCPF = new ValidarCPF(cpf);
+
+console.log(validarCPF.iniciar());
---
+++ js/validar-cpf.js
@@ -0,0 +1,33 @@
+export default class ValidarCPF {
+  constructor(element) {
+    this.element = element;
+  }
+  limpar(cpf) {
+    return cpf.replace(/\D/g, '');
+  }
+  construir(cpf) {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+  }
+  formatar(cpf) {
+    const cpfLimpo = this.limpar(cpf);
+    return this.construir(cpfLimpo);
+  }
+  validar(cpf) {
+    const matchCPF = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/g)
+    return (matchCPF && matchCPF[0] == cpf);
+  }
+  validarNaMudanca(cpfElement) {
+    if (this.validar(cpfElement.value)) {
+      cpfElement.value = this.formatar(cpfElement.value);
+    } else { }
+  }
+  adicionarEvento() { // () => { this = ValidarCPF } | function() { this = element }
+    this.element.addEventListener('change', () => {
+      this.validarNaMudanca(this.element);
+    });
+  }
+  iniciar() {
+    this.adicionarEvento();
+    return this;
+  }
+}
